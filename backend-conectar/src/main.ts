14250cabf6815affce05e588556663a52cb38e7a
jnerydesigner <jander.webmaster@gmail.com>
2025-06-19 14:59:09 -0400

    Feat: connection success swagger

--- backend-conectar/src/main.ts
+++ backend-conectar/src/main.ts
@@ -2,12 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './infra/modules/app.module';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new ConfigService();
   const PORT = config.get<number>('SERVER_PORT', 3334);
   const logger = new Logger('Initial Application');
+  const configSwagger = new DocumentBuilder()
+    .setTitle('Conectar Authentication and Creation Users')
+    .setDescription(
+      'This is Documentation, api connect and Resource User, Authentication to Conecta',
+    )
+    .setVersion('1.0')
+    .addTag('conecta')
+    .build();
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(PORT, () => {
     logger.log(`Server Running in PORT = ${PORT}`);
   });
